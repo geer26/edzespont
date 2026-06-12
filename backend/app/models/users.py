9719f03b8648645f45d1dc7baf881ae2d0735989
geer26 <gergo.kurucz@gmail.com>
2026-06-12 18:16:17 +0200

    alembic

---
+++ backend/app/models/users.py
@@ -0,0 +1,34 @@
+from sqlmodel import Field, SQLModel
+from sqlalchemy import Column, DateTime, func, String
+from datetime import datetime
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: int | None = Field(default=None, primary_key=True)
+
+    username: str = Field(
+        sa_column=Column(String(50), unique=True, index=True, nullable=False)
+    )
+
+    email: str = Field(
+        sa_column=Column(String(255), unique=True, index=True, nullable=False)
+    )
+
+    password_hash: str = Field(nullable=False)
+
+    is_active: bool = Field(default=True, nullable=False)
+
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
+    )
+
+    updated_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=False),
+            server_default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+        )
+    )
