68b823508fa3c756ce4bbf943baffbb32bf824de
geer26 <gergo.kurucz@gmail.com>
2026-06-12 06:10:35 +0200

    fastapi backend

--- rbe/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod res;
-pub mod db;
-
-// use std::io::Error;
-// use mongodb::error::Error;
-// use crate::res::{structs, roommate};
--- rbe/src/res.rs
+++
@@ -1,6 +0,0 @@
-pub mod structs;
-pub mod roommate;
-pub mod roomspectator;
-pub mod workout;
-pub mod user;
-pub mod config;
