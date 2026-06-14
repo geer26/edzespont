1dc725ff6b316e833e4bbdbad1925d14f6840f8c
geer26 <gergo.kurucz@gmail.com>
2026-06-14 19:50:42 +0200

    upd

--- backend/app/api/tests.py
+++ backend/app/api/tests.py
@@ -2,19 +2,17 @@ from fastapi import APIRouter, Request, Body, Cookie, Header
 from app.core.config import SettingsDep
 from app.core.db import DbSessionDep
 from app.models import User
-from app.services import user_service
+from app.api.tags import Tags
 
 
 tests_router = APIRouter()
 
 
-@tests_router.get("/", tags=["root"])
+@tests_router.get("/", tags=[Tags.test])
 async def read_root(settings: SettingsDep) -> dict:
     return {"message": "Welcome to edzespont!", "path": settings.path}
 
 
-@tests_router.post("/", tags=["root"])
+@tests_router.post("/", tags=[Tags.test])
 async def write_root() -> dict:
     return {"message": "Write OK!"}
-
-
