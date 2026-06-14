5a85570780a900cd1a77372f89c4e9c040a57c79
geer26 <gergo.kurucz@gmail.com>
2026-06-14 19:51:03 +0200

    Update user.py

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -2,26 +2,27 @@ from fastapi import APIRouter, Request, Body, Cookie, Header, status
 from app.core.db import DbSessionDep
 from app.schemas import user_schema
 from app.services import user_service
+from app.api.tags import Tags
 
 
 user_router = APIRouter()
 
 
-@user_router.get("/users", response_model=list[user_schema.UserRead])
+@user_router.get("/users", response_model=list[user_schema.UserRead], tags=[Tags.user])
 async def read_all_user(session: DbSessionDep):
     return await user_service.get_all_users(session)
 
 
-@user_router.get("/users/{userid}", response_model=user_schema.UserRead)
+@user_router.get("/users/{userid}", response_model=user_schema.UserRead, tags=[Tags.user])
 async def get_user_by_id(userid: int, session: DbSessionDep):
     return await user_service.get_user_by_id(userid, session)
 
 
-@user_router.post("/users", response_model=list[user_schema.UserRead], status_code=status.HTTP_201_CREATED)
+@user_router.post("/users", response_model=list[user_schema.UserRead], status_code=status.HTTP_201_CREATED, tags=[Tags.user])
 async def add_user(user: user_schema.UserCreate, session: DbSessionDep):
     return await user_service.add_user(user, session)
 
 
-@user_router.delete("/users/{userid}", response_model=list[user_schema.UserRead])
+@user_router.delete("/users/{userid}", response_model=list[user_schema.UserRead], tags=[Tags.user])
 async def delete_user(userid: int, session: DbSessionDep):
     return await user_service.delete_user(userid, session)
